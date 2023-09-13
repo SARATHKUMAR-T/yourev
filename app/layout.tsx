2b683265248d583083d152288d543d329114ee7c
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-09-13 17:20:22 +0530

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 "use client";
 import "./globals.css";
-import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const mont = Montserrat({
   subsets: ["latin"],
 });
-
-export const metadata: Metadata = {
-  title: "zemo",
-  description: "self development app",
-};
 
 const queryClient = new QueryClient();
 
